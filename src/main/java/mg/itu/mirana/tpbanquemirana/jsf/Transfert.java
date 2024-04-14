144d579c244a91e6f4a79985bf96db815de6e345
MiranaIns <89468422+MiranaIns@users.noreply.github.com>
2024-04-14 22:14:33 +0300

    Nouvelles fonctionnalités : transferts entre comptes: Traiter les erreurs et messages de succès

--- src/main/java/mg/itu/mirana/tpbanquemirana/jsf/Transfert.java
+++ src/main/java/mg/itu/mirana/tpbanquemirana/jsf/Transfert.java
@@ -8,6 +8,7 @@ import jakarta.inject.Named;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import mg.itu.mirana.tpbanquemirana.entity.CompteBancaire;
+import mg.itu.mirana.tpbanquemirana.jsf.util.Util;
 import mg.itu.mirana.tpbanquemirana.service.GestionnaireCompte;
 
 /**
@@ -56,9 +57,34 @@ public class Transfert {
     }
 
     public String enregistrer() {
+        boolean erreur = false;
+
         CompteBancaire source = gestionnaireCompte.findById(idSource);
+        if (source == null) {
+            // Message d'erreur associé au composant source ; form:source est l'id client
+            // si l'id du formulaire est "form" et l'id du champ de saisie de l'id de la source est "source"
+            // dans la page JSF qui lance le transfert.
+            Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:source");
+            erreur = true;
+        } else {
+            if (source.getSolde() < montant) {
+                Util.messageErreur("Solde de " + source.getNom() + " insuffisant", "Solde insuffisant", "form:montant");
+                erreur = true;
+            }
+        }
+
         CompteBancaire destination = gestionnaireCompte.findById(idDestination);
+        if (destination == null) {
+            Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:destination");
+            erreur = true;
+        }
+
+        if (erreur) { // en cas d'erreur, rester sur la même page
+            return null;
+        }
+
         gestionnaireCompte.transferer(source, destination, montant);
+        Util.addFlashInfoMessage("Transfert de " + source.getNom() + " vers " + destination.getNom() + " pour un montant de " + montant + " correctement effectué");
         return "listeComptes?faces-redirect=true";
 
     }
